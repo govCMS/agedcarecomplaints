84789748094479c67efe3edbbc96b2d32d23db4d
Joseph Zhao <joseph.zhao@xing.net.au>
2017-02-03 09:07:15 +1100

    Add scripts for FAQs page

--- govstrap/js/scripts.js
+++ govstrap/js/scripts.js
@@ -1,13 +1,13 @@
-jQuery(function($) {
+jQuery(function ($) {
 
 // Document ready...
-    $(document).ready(function(){
+    $(document).ready(function () {
 
         // Table Row Zebra stripes
         $("table tr:odd").addClass("zebra");
 
         // Add the Mobile Navigation show/hide expanders
-        $('#mov-nav .sf-main-menu ul > li').each(function() {
+        $('#mov-nav .sf-main-menu ul > li').each(function () {
             if ($(this).children('ul').length > 0) {
                 $(this).prepend('<a href="javascript:void(0)" class="show-hide"><span class="offleft">Show/hide children</span></a> ');
             }
@@ -16,24 +16,24 @@ jQuery(function($) {
         $('#mov-nav .sf-main-menu .show-hide').hide();
 
         // Mobile Navigation show/hide expanders
-        $('.show-hide').click(function(){
-            if($(this).hasClass('expanded')) {
+        $('.show-hide').click(function () {
+            if ($(this).hasClass('expanded')) {
                 $(this).removeClass('expanded');
-                $(this).parent('li').children('ul').slideUp(800,'easeOutExpo');
+                $(this).parent('li').children('ul').slideUp(800, 'easeOutExpo');
             }
             else {
                 $(this).addClass('expanded');
-                $(this).parent('li').children('ul').slideDown(1000,'easeOutExpo');
+                $(this).parent('li').children('ul').slideDown(1000, 'easeOutExpo');
             }
         });
 
         // Change the Gravity Forms required asterix into text
-        $('.gfield_required').each(function() {
+        $('.gfield_required').each(function () {
             $(this).html(' (required)');
         });
 
         // Remove the tabindex attribs off Gravity Form inputs
-        $(':input').each(function() {
+        $(':input').each(function () {
             $(this).removeAttr('tabindex');
         });
 
@@ -41,10 +41,10 @@ jQuery(function($) {
         $('.gfield .textarea').removeAttr('cols');
 
         //Validate search form
-        $('.search-form input[type="submit"]').click(function(e){
-            if($('.search-form .search-field').val() == '') {
+        $('.search-form input[type="submit"]').click(function (e) {
+            if ($('.search-form .search-field').val() == '') {
                 e.preventDefault();
-                if($('.search-form-error').length === 0){
+                if ($('.search-form-error').length === 0) {
                     $('.search-form').append('<div class="search-form-error">Keyword field cannot be empty</div>');
                     $('.search-form-error').fadeIn('slow');
                 }
@@ -52,19 +52,19 @@ jQuery(function($) {
         });
 
         // Fieldset expanders
-        $('#gform_wrapper_5 fieldset fieldset').each(function() {
+        $('#gform_wrapper_5 fieldset fieldset').each(function () {
             $(this).children('legend').append('<a href="javascript:void(0)" class="show-hide-field"><span class="tri"></span><span class="offleft">Show/hide children</span></a> ');
         });
 
         // Fieldset expanders
-        $('.show-hide-field').click(function(){
-            if($(this).hasClass('expanded')) {
+        $('.show-hide-field').click(function () {
+            if ($(this).hasClass('expanded')) {
                 $(this).removeClass('expanded');
-                $(this).parent('legend').parent('fieldset').children('ul').slideUp(800,'easeOutExpo');
+                $(this).parent('legend').parent('fieldset').children('ul').slideUp(800, 'easeOutExpo');
             }
-            else{
+            else {
                 $(this).addClass('expanded');
-                $(this).parent('legend').parent('fieldset').children('ul').slideDown(1000,'easeOutExpo');
+                $(this).parent('legend').parent('fieldset').children('ul').slideDown(1000, 'easeOutExpo');
             }
         });
 
@@ -81,14 +81,19 @@ jQuery(function($) {
         timeout: 300,
         out: makeShort
     });
-    function makeTall(){$(this).children('.sub-menu').slideDown(300, 'easeInSine');}
-    function makeShort(){$(this).children('.sub-menu').slideUp(200, 'easeInSine')}
+    function makeTall() {
+        $(this).children('.sub-menu').slideDown(300, 'easeInSine');
+    }
+
+    function makeShort() {
+        $(this).children('.sub-menu').slideUp(200, 'easeInSine')
+    }
 
 // Mobile Navigation
-    $('#_navigation').click(function(){
-        if($('#mob-nav').hasClass('active')) {
+    $('#_navigation').click(function () {
+        if ($('#mob-nav').hasClass('active')) {
             $(this).removeClass('active');
-            $('#mob-nav').slideUp(500, 'easeInSine', function(){
+            $('#mob-nav').slideUp(500, 'easeInSine', function () {
                 $(this).removeClass('active');
                 $('#mob-nav li ul').removeClass('mob-sub-menu').addClass('sub-menu');
                 $('#mov-nav .sf-main-menu .show-hide').hide();
@@ -103,17 +108,16 @@ jQuery(function($) {
     });
 
 // FAQs, pseudo accordion.
-    $(document).ready(function(){
+    $(document).ready(function () {
         //$('.faqs dt a').first().addClass('active');
-        $('.faqs dd').each(function() {
+        $('.faqs dd').each(function () {
             $(this).hide();
         });
-        //$('.faqs dd').first().show();
     });
-    $('.faqs dt a').click(function(event){
+    $('.faqs dt a').click(function (event) {
         event.preventDefault();
         var tarhash = $(this).attr('href');
-        if(tarhash) {
+        if (tarhash) {
             $('.faqs dd').slideUp(500, 'easeInSine');
             $(tarhash).slideDown(500, 'easeInSine');
         }
@@ -122,49 +126,95 @@ jQuery(function($) {
     });
 
 //Sitemap, pseudo accordion.
-    $(document).ready(function(){
-        $('.site-map ul > li').each(function() {
+    $(document).ready(function () {
+        $('.site-map ul > li').each(function () {
             if ($(this).children('ul').length > 0) {
                 $(this).prepend('<a href="javascript:void(0)" class="smshow-hide"><span class="offleft">Show/hide children</span></a> ');
                 $(this).children('ul').hide();
             }
         });
-        $('.smshow-hide').click(function(){
-            if($(this).hasClass('expanded')) {
+        $('.smshow-hide').click(function () {
+            if ($(this).hasClass('expanded')) {
                 $(this).removeClass('expanded');
-                $(this).parent('li').children('ul').slideUp(800,'easeOutExpo');
+                $(this).parent('li').children('ul').slideUp(800, 'easeOutExpo');
             }
             else {
                 $(this).addClass('expanded');
-                $(this).parent('li').children('ul').slideDown(1000,'easeOutExpo');
+                $(this).parent('li').children('ul').slideDown(1000, 'easeOutExpo');
             }
         });
     });
 
 //turn on fitvids
-    $(document).ready(function(){
+    $(document).ready(function () {
         // Target your .container, .wrapper, .post, etc.
         $("#content").fitVids();
     });
 
 //Get zindexing for iframes to work
     $(document).ready(function () {
-        $('iframe').each(function(){
+        $('iframe').each(function () {
             var url = $(this).attr("src");
-            $(this).attr("src",url+"?wmode=transparent&rel=0");
+            $(this).attr("src", url + "?wmode=transparent&rel=0");
         });
     });
 
 //Front page slider
     $('.slider-container').cycle({
         fx: 'fade',
-        speed:  4000,
+        speed: 4000,
     });
 
 // Remove the tabindex attribs off Gravity Form inputs
-    $(':input').each(function() {
+    $(':input').each(function () {
         $(this).removeAttr('tabindex');
     });
 
 
 });
+
+/**
+ * @file
+ * A JavaScript file for the Site.
+ */
+
+(function ($, Drupal) {
+
+    'use strict';
+
+    var faqAction;
+
+    Drupal.behaviors.govstrap = {
+        attach: function (context, settings) {
+            faqAction();
+        }
+    };
+
+    /**
+     * FAQs page.
+     */
+    faqAction = function () {
+        var faqs;
+        var faqTarget;
+
+        faqs = $('div.faq');
+        faqTarget = $('.faq-list h4 a');
+
+        // Hide all the faq details.
+        faqs.hide();
+
+        // Open faq details.
+        faqTarget.click(function (event) {
+            event.preventDefault();
+            var faq = $(this).parent().next();
+            // Close all.
+            faqs.slideUp(500, 'easeInSine');
+            // Open faq.
+            faq.slideDown(500, 'easeInSine');
+            // Apply class.
+            faqTarget.removeClass('active');
+            $(this).addClass('active');
+        });
+    };
+
+})(jQuery, Drupal);
